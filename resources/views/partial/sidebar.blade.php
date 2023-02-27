3f6ee32d112b7e479aa4a3479a0350f70c3688a6
VickyLeonardoo <vickyleonardo23@gmail.com>
2023-02-27 13:22:13 +0700

    FE Edit

--- resources/views/partial/sidebar.blade.php
+++ resources/views/partial/sidebar.blade.php
@@ -83,6 +83,36 @@
                   </p>
                 </a>
             </li>
+            <li class="nav-item ">
+                <a href="#" class="nav-link ">
+                  <i class="nav-icon fas fa-tachometer-alt"></i>
+                  <p>
+                    Keuangan
+                    <i class="right fas fa-angle-left"></i>
+                  </p>
+                </a>
+                <ul class="nav nav-treeview">
+                <li class="nav-item">
+                    <a class="nav-link {{ request()->is('data-pendeta','tambah-data-pendeta') ? 'active' : '' }}" href="/data-pendeta">
+                      <i class="far fa-circle nav-icon"></i>
+                      <p>Data Master</p>
+                    </a>
+                  </li>
+
+                  <li class="nav-item">
+                    <a class="nav-link {{ request()->is('data-wijk','tambah-data-wijk','data-wijk-anggota-wijk-') ? 'active' : ''}} " href="/data-wijk">
+                      <i class="far fa-circle nav-icon"></i>
+                      <p>Deposit</p>
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link {{ request()->is('data-sintua','tambah-data-sintua') ? 'active' : ''}} " href="/data-sintua">
+                      <i class="far fa-circle nav-icon"></i>
+                      <p>Pengeluaran</p>
+                    </a>
+                  </li>
+                </ul>
+            </li>
         </ul>
       </nav>
       <!-- /.sidebar-menu -->
