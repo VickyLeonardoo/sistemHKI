e448a43c7ad979fe2595f9bfd33aab07fa4e6c97
Vickyleonardoo <vickyleonardo23@gmail.com>
2023-10-24 17:32:13 +0800

    tes

--- resources/views/bph/partials/sidebar.blade.php
+++ resources/views/bph/partials/sidebar.blade.php
@@ -1,6 +1,6 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="#" class="brand-link">
+    <a href="/bph" class="brand-link">
         <span class="brand-text font-weight-light">SIM HKI</span>
     </a>
 
